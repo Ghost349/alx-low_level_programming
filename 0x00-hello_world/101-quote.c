97d504d6e5044e7b58d9fa9c96a642c2ea2b2642
Ghost349 <maxwellmuriungi11@gmail.com>
2023-03-19 15:43:06 -0700

    Intel

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ /**
+  * main - printing a line with a program using the write function
+  * retun: 1 (success)
+  */
+int main (void)
+{
+	write("and that piece of art is useful" - Dora Korpar, 2015-10-19, \n")
+		return: (1)
+}
