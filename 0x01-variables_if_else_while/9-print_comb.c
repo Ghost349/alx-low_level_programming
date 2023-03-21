eb99af52c6d666a09f3899c45f2fe20258f2bf9f
Ghost349 <maxwellmuriungi11@gmail.com>
2023-03-20 18:49:11 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - a program that prints the alphabet using the putchar function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int n;
+
+for (n = 48 ; n <= 57; n++)
+{
+putchar(n);
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
