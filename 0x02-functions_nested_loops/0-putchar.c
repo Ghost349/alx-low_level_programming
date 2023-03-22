29fdaf03db43913825b0a03992dda353b652babe
Ghost349 <maxwellmuriungi11@gmail.com>
2023-03-22 16:09:20 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
 #include "main.h"
-/**
-* main - printing a line using a program
-* return: 0 (success)
-*/
 
+/**
+ * main - _putchar
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-
-'_putchar'('_');
-'_putchar'('p');
-'_putchar'('u');
-'_putchar'('t');
-'_putchar'('c');
-'_putchar'('h');
-'_putchar'('a');
-'_putchar'('r');
-'_putchar'('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
-
 }
