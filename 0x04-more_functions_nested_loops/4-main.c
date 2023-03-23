5862ac1d74d6e2e67b9c7650a049782c332676b2
Ghost349 <maxwellmuriungi11@gmail.com>
2023-03-23 15:23:55 -0700

    I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-main.c
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -9,15 +9,4 @@ int main(void)
 {
     print_most_numbers();
     return (0);
-}#include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_most_numbers();
-    return (0);
 }
