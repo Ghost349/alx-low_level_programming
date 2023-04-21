8f58e64973b1d7d1bbbab998dd82bd0776aec2b8
Ghost349 <maxwellmuriungi11@gmail.com>
2023-04-21 08:47:13 -0700

    To hell with circumstances; I create opportunities

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,11 +1,11 @@
 #include "function_pointers.h"
 /**
-*int_index - a function that searches for an integer
-*array: array
-*cmp: pointer to function of one of the 3 main
-*int_index- returns the index of the first element
-*Return 0
-*/
+ * int_index - a function that searches for an integer
+ * @array: array
+ * @cmp: pointer to function of one of the 3 main
+ * @size: size of the array
+ * Return: 0
+ */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int gm;
