5462475a7c557c4359bb4492d976855f7f282a6f
Ghost349 <maxwellmuriungi11@gmail.com>
2023-05-02 04:53:29 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -5,6 +5,7 @@
  * str_concat - a function that gets ends of input and add together for size
  * @s1: input one to concat
  * @s2: input two to concat
+ *
  * Return: concat of s1 and s2
  */
 
@@ -19,6 +20,7 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		return ("");
 
+	red = gin = 0;
 	while (s1[red] != '\0')
 	{
 		red++;
