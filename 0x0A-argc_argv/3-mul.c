e9ad794531fe7a2c5d92cc84981c224db192304a
Ghost349 <maxwellmuriungi11@gmail.com>
2023-05-02 04:25:51 -0700

    Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,49 +7,63 @@
  */
 int _atoi(char *s)
 {
-	int max, bob, t, len, g, git = 0;
+	int max, bob, t, len, g, git;
+
+	max = 0;
+	bob = 0;
+	t = 0;
+	len = 0;
+	g = 0;
+	git = 0;
 
 	while (s[len] != '\0')
 	len++;
+
 	while (max < len && g == 0)
 	{
-	if (s[max] == '-')
-	++bob;
-	if (s[max] >= '0' && s[max] <= '9')
-	{
-	git = s[max] - '0';
-	if (bob % 2)
-	git = -git;
-	t = t * 10 + git;
-	g = 1;
-	if (s[max + 1] < '0' || s[max + 1] > '9')
-	break;
-	g = 0;
-	}
-	max++;
+		if (s[max] == '-')
+			++bob;
+
+		if (s[max] >= '0' && s[max] <= '9')
+		{
+			git = s[max] - '0';
+			if (bob % 2)
+				git = -git;
+			t = t * 10 + git;
+			g = 1;
+			if (s[max + 1] < '0' || s[max + 1] > '9')
+				break;
+			g = 0;
+		}
+		max++;
 	}
 	if (g == 0)
-	return (0);
+		return (0);
+
 	return (t);
 }
 /**
  * main - a function that multiplies two numbers
  * @argc: number of arguments
  * @argv: array of arguments
+ *
  * Return: 0 (Success), 1 (Error)
  */
 int main(int argc, char *argv[])
 {
-	int result, num3, num4;
+	int result, num1, num2;
 
 	if (argc < 3 || argc > 3)
 	{
-	printf("Error\n");
+		printf("Error\n");
 	return (1);
 	}
-	num3 = _atoi(argv[1]);
-	num4 = _atoi(argv[2]);
-	result = num3 * num4;
+
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = num1 * num2;
+
 	printf("%d\n", result);
+
 	return (0);
 }
