60a16ce6ac9c196340dac87810f133691a720549
Ghost349 <maxwellmuriungi11@gmail.com>
2023-05-02 04:47:56 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * str_concat - get ends of input and add together for size
+ * str_concat - a function that gets ends of input and add together for size
  * @s1: input one to concat
  * @s2: input two to concat
  * Return: concat of s1 and s2
@@ -11,31 +11,35 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *conct;
-	int red, gin;
+	int red = 0;
+	int gin = 0;
 
 	if (s1 == NULL)
-	s1 = "";
+		return ("");
 	if (s2 == NULL)
-	s2 = "";
-	red = 0;
-	gin = 0;
-	while (s1[red] != '\0')
-	red++;
-	while (s2[gin] != '\0')
-	gin++;
-	conct = malloc(sizeof(char) * (red + gin + 1));
-	if (conct == NULL)
-	return (NULL);
-	red = 0;
-	gin = 0;
+		return ("");
+
 	while (s1[red] != '\0')
 	{
-	conct[red] = s1[red];
-	red++;
+		red++;
+	}
+
+	while (s2[gin] != '\0')
+		gin++;
+
+	conct = malloc(sizeof(char) * (red + gin + 1));
+
+	if (conct == NULL)
+		return (NULL);
+	gin = red = 0;
+	while (s1[red] != '\0')
+	{
+		conct[red] = s1[red];
+		red++;
 	}
 	while (s2[gin] != '\0')
 	{
-	conct[red] = s2[gin];
+		conct[red] = s2[gin];
 	red++, gin++;
 	}
 	conct[red] = '\0';
