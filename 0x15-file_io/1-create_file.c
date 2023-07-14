a5407a6e3430f611ea1ddfd90c59c401ed192e4b
Ghost349 <maxwellmureti@gmail.com>
2023-07-14 14:49:22 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,29 +12,28 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int max = 0;
-	int kez = 0;
-	int length = 0;
+	int mx, ke, length = 0;
 
-	if (max == -1)
-		return (-1);
 
 	if (filename == NULL)
 		return (-1);
 
-	max = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	mx = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (mx == -1)
+		return (-1);
 
 	if (text_content == NULL)
 		return (0);
 
 	for (length = 0; text_content[length]; length++)
 
-	kez = write(max, text_content, length);
+	ke = write(mx, text_content, length);
 
-	if (kez == -1)
+	if (ke == -1)
 		return (-1);
 
-	close(max);
+	close(mx);
 
 	return (1);
 }
