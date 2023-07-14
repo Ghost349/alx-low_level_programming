2cd93656a2350469174e0e928503f5d10d9d3412
Ghost349 <maxwellmureti@gmail.com>
2023-07-14 14:52:32 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
-		return (0);
+		text_content = "";
 
 	for (length = 0; text_content[length]; length++)
 
